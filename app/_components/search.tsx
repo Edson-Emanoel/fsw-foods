c237f8c5c8f2ba53f4d681103f0c1ee29e3821d7
Edson-Emanoel <edson.emanoelofc@gmail.com>
2025-08-05 15:57:28 -0300

    feat: Adicionei o arquivo prisma.ts

---
+++ app/_components/search.tsx
@@ -0,0 +1,17 @@
+import { SearchIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+const Search = () => {
+    return (
+        <div className="flex gap-2">
+            <Input placeholder="Buscar Restaurantes" className="border-none" />
+
+            <Button size="icon" className="w-10 h-10">
+                <SearchIcon size={20} />
+            </Button>
+        </div>
+    );
+}
+ 
+export default Search;
