d93115753d55ce0a54e5baf2811f571dfb4c26a9
Edson-Emanoel <edson.emanoelofc@gmail.com>
2025-08-10 16:10:03 -0300

    refactor: Garanti que a lista de produtos receba os produtos via props

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,26 @@ import Image from "next/image";
 import ProductList from "./_components/products-list";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
+import { db } from "./_lib/prisma";
 
-export default function Home() {
+const Home = async () => {
+
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      }
+    },
+    take: 10, // Pega apenas 10 registros
+    include: { // Inclui o "registro" do restaurante
+      restaurant: {
+        select: {
+          name: true
+        }
+      }
+    }
+  })
+
   return (
     <>
       <Header />
@@ -41,8 +59,10 @@ export default function Home() {
             <ChevronRightIcon size={16} />
           </Button>
         </div>
-        <ProductList />
+        <ProductList products={products} />
       </div>
     </>
   );
 }
+
+export default Home;
