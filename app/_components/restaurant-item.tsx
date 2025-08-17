f65409db48c36bf19d6aeb357d0641290ca69fd6
Edson-Emanoel <edson.emanoelofc@gmail.com>
2025-08-17 10:52:28 -0300

    feat: Comecei a criar a página de restaurante

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -3,6 +3,7 @@ import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
 import Image from "next/image";
 import { formatCurrency } from "../_helpers/price";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 interface RestaurantItemProps{
     restaurant: Restaurant
@@ -10,49 +11,54 @@ interface RestaurantItemProps{
 
 const RestaurantItem = ({restaurant}: RestaurantItemProps) => {
     return  (
-        <div className="min-w-[266px] max-w-[266px] space-y-3">
-            {/* IMAGEM */}
-            <div className="w-full h-[136px] relative">
-                <Image src={restaurant.imageUrl} alt={restaurant.name} fill className="rounded-lg object-cover" />
+        <Link
+            className="min-w-[266px] max-w-[266px] space-y-3"
+            href={`/restaurants/${restaurant.id}`}
+        >
+            <div className="min-w-[266px] max-w-[266px] space-y-3">
+                {/* IMAGEM */}
+                <div className="w-full h-[136px] relative">
+                    <Image src={restaurant.imageUrl} alt={restaurant.name} fill className="rounded-lg object-cover" />
 
-                {/* Avaliação */}
-                <div className="absolute gap-[2px] top-2 left-2 bg-white px-2 py-[2px] rounded-full flex items-center text-white">
-                    <StarIcon size={12} className="fill-yellow-500 text-yellow-500" />
-                    <span className="text-xs font-semibold">5.0</span>
-                </div>
-
-                <Button size="icon" className="absolute w-7 h-7 right-2 top-2 bg-white/20 rounded-full">
-                    <HeartIcon className="fill-white" size={16} />
-                </Button>
-            </div>
-
-            {/* TEXTO */}
-            <div className="">
-                <h3 className="text-sm font-semibold">{restaurant.name}</h3>
-                {/* INFORMAÇÕES DA ENTREGA */}
-                <div className="flex gap-3">
-                    {/* ENTREGA */}
-                    <div className="flex items-center gap-1">
-                        <BikeIcon className="text-primary" size={14} />
-
-                        <span className="text-xs text-muted-foreground">
-                            {Number(restaurant.deliveryFee) === 0
-                                ? "Entrega Gratuita"
-                                : formatCurrency(Number(restaurant.deliveryFee)) }
-                        </span>
+                    {/* Avaliação */}
+                    <div className="absolute gap-[2px] top-2 left-2 bg-white px-2 py-[2px] rounded-full flex items-center text-white">
+                        <StarIcon size={12} className="fill-yellow-500 text-yellow-500" />
+                        <span className="text-xs font-semibold">5.0</span>
                     </div>
 
-                    {/* TEMPO DE ENTREGA */}
-                    <div className="flex items-center gap-1">
-                        <TimerIcon className="text-primary" size={14} />
+                    <Button size="icon" className="absolute w-7 h-7 right-2 top-2 bg-white/20 rounded-full">
+                        <HeartIcon className="fill-white" size={16} />
+                    </Button>
+                </div>
 
-                        <span className="text-xs text-muted-foreground">
-                        {restaurant.deliveryTimeMinutes} min
-                        </span>
+                {/* TEXTO */}
+                <div className="">
+                    <h3 className="text-sm font-semibold">{restaurant.name}</h3>
+                    {/* INFORMAÇÕES DA ENTREGA */}
+                    <div className="flex gap-3">
+                        {/* ENTREGA */}
+                        <div className="flex items-center gap-1">
+                            <BikeIcon className="text-primary" size={14} />
+
+                            <span className="text-xs text-muted-foreground">
+                                {Number(restaurant.deliveryFee) === 0
+                                    ? "Entrega Gratuita"
+                                    : formatCurrency(Number(restaurant.deliveryFee)) }
+                            </span>
+                        </div>
+
+                        {/* TEMPO DE ENTREGA */}
+                        <div className="flex items-center gap-1">
+                            <TimerIcon className="text-primary" size={14} />
+
+                            <span className="text-xs text-muted-foreground">
+                            {restaurant.deliveryTimeMinutes} min
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
  
--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -1,10 +1,23 @@
+import { db } from "@/app/_lib/prisma";
+import { notFound } from "next/navigation";
+
 interface RestaurantsPageProps {
     params: {
         id: string;
     }
 }
 
-const RestaurantsPage = ({ params }: RestaurantsPageProps) => {
+const RestaurantsPage = async ({ params: { id } }: RestaurantsPageProps) => {
+    const restaurant = await db.restaurant.findUnique({
+        where: {
+            id: id
+        }
+    })
+
+    if (!restaurant){
+        return notFound()
+    }
+
     return (
         <h1>Restaurante: {params.id}</h1>
     );
