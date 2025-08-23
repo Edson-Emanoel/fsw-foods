943736e0b1d89ca931529941a3a1a1b1b2492e0a
Edson-Emanoel <edson.emanoelofc@gmail.com>
2025-08-23 20:03:51 -0300

    refactor: Movi os Links para dentro de Botões

--- app/page.tsx
+++ app/page.tsx
@@ -48,15 +48,16 @@ const Home = async () => {
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5 mb-2">
           <h2 className="font-semibold">Produtos Recomendados</h2>
-          <Link href="/products/recommended">
             <Button
               variant="ghost"
               className="h-fit text-primary p-0 hover:bg-transparent"
+              asChild
             >
-              Ver Todos
-              <ChevronRightIcon size={16} />
+              <Link href="/products/recommended">
+                Ver Todos
+                <ChevronRightIcon size={16} />
+              </Link>
             </Button>
-          </Link>
         </div>
         <ProductList products={products} />
       </div>
@@ -69,15 +70,15 @@ const Home = async () => {
         <div className="flex items-center justify-between px-5 mb-2">
           <h2 className="font-semibold">Restaurantes Recomendados</h2>
           
-          <Link href="/restaurants/recommended">
             <Button
               variant="ghost"
               className="h-fit text-primary p-0 hover:bg-transparent"
             >
-              Ver Todos
-              <ChevronRightIcon size={16} />
+              <Link href="/restaurants/recommended">
+                Ver Todos
+                <ChevronRightIcon size={16} />
+              </Link>
             </Button>
-          </Link>
         </div>
         <RestaurantList />
       </div>
